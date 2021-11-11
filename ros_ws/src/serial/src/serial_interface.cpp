34f0d6132607a8826639c7bc398dcd884ce7cab5
Sébastien Darche <sebastien.darche@free.fr>
2021-11-11 12:02:25 -0500

    Init serial node

---
+++ ros_ws/src/serial/src/serial_interface.cpp
@@ -0,0 +1,7 @@
+/** \file serial_interface.cpp
+ * \brief Serial interface node executable
+ *
+ * \author Sébastien Darche <sebastien.darche@polymtl.ca>
+ */
+
+int main(int argc, char** argv) { return 0; }
