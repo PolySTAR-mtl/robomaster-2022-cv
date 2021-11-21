c24fa36429656fcc37cb8a42cb6c95e4e344a3e6
Sébastien Darche <sebastien.darche@free.fr>
2021-11-20 22:09:36 -0500

    Renamed nodes, launch file && monitor_logger base

--- ros_ws/src/monitor/src/monitor_video.cpp
+++ ros_ws/src/monitor/src/monitor_video.cpp
@@ -1,4 +1,4 @@
-/** \file video_monitor.cpp
+/** \file monitor_video.cpp
  * \brief Video monitor node executable
  *
  * \author Sébastien Darche <sebastien.darche@polymtl.ca>
@@ -8,13 +8,12 @@
 
 #include <ros/ros.h>
 
-
-/** \brief This node shows the current video stream as well as the detected bounding boxes
+/** \brief This node shows the current video stream as well as the detected
+ * bounding boxes, tracklets and current target
  */
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "video_monitor");
+    ros::init(argc, argv, "monitor_video");
     ros::NodeHandle nh;
 
     ros::spin();
 }
-
