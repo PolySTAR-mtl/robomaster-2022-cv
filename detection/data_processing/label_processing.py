f4d0760a732603bc0b86159ea6637720cc90096f
FlowSs <tambonflorian01@gmail.com>
2021-11-24 10:55:35 -0500

    Modified label_processing.py to take into account the structure of the project

--- detection/data_processing/label_processing.py
+++ detection/data_processing/label_processing.py
@@ -4,11 +4,12 @@ import xml.etree.ElementTree as ET
 import pickle
 import os
 from os import listdir, getcwd
-from os.path import join
+from os.path import join, isfile, splitext
 
-sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
-
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+# Directories name
+sets=['robomaster_Central China Regional Competition', 'robomaster_Final Tournament', 'robomaster_North China Regional Competition', 'robomaster_South China Regional Competition']
+# List of classes
+classes = ["red_armor", "blue_armor", "grey_armor", "base", "watcher", "car"]
 
 
 def convert(size, box):
@@ -24,38 +25,57 @@ def convert(size, box):
     h = h*dh
     return (x,y,w,h)
 
-def convert_annotation(year, image_id):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    out_file = open('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
+def convert_annotation(set_name, image_id):
+    in_file = open('../dataset/%s/image_annotation/%s.xml'%(set_name, image_id))
+
     tree=ET.parse(in_file)
     root = tree.getroot()
+    
+    # If filename in annotation is different from the image name
+    if(root.find('filename').text != image_id+".jpg"):
+    	return False
+
+    out_file = open('../dataset/%s/labels/%s.txt'%(set_name, image_id), 'w')
+
     size = root.find('size')
     w = int(size.find('width').text)
     h = int(size.find('height').text)
 
     for obj in root.iter('object'):
-        difficult = obj.find('difficult').text
+        difficult = 0
+        if obj.find('difficulty') is not None:
+          difficult = obj.find('difficulty').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
+        cls_color = ""
+        if obj.find('armor_color') is not None:
+          cls_color = obj.find('armor_color').text + "_"
+        if cls_color+cls not in classes or int(difficult)>1:
             continue
-        cls_id = classes.index(cls)
+        cls_id = classes.index(cls_color+cls)
         xmlbox = obj.find('bndbox')
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
         bb = convert((w,h), b)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
-wd = getcwd()
+    return True
 
-for year, image_set in sets:
-    if not os.path.exists('VOCdevkit/VOC%s/labels/'%(year)):
-        os.makedirs('VOCdevkit/VOC%s/labels/'%(year))
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
-    for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
-        convert_annotation(year, image_id)
+if __name__ == "__main__":
+
+  for set_name in sets:
+    print("Processing %s..."%(set_name))
+    if not os.path.exists('../dataset/%s'%(set_name)):
+        print("No directory %s"%(set_name))
+        continue
+    if not os.path.exists('../dataset/%s/labels/'%(set_name)):
+        os.makedirs('../dataset/%s/labels/'%(set_name))
+    image_ids = [f for f in listdir('../dataset/%s/image/'%(set_name)) if isfile(join('../dataset/%s/image/'%(set_name), f))]
+    image_ids_ = [splitext(f)[0] for f in image_ids]
+    list_file = open('../dataset/%s/%s.txt'%(set_name, set_name), 'w')
+    for i, image_id in enumerate(image_ids_):
+        if((i/len(image_ids)*100)%10==0):
+            print("{:0.2f}% done".format(i/len(image_ids)*100))
+        if(convert_annotation(set_name, image_id)):
+            list_file.write('../dataset/%s/image/%s.jpg\n'%(set_name, image_id))
     list_file.close()
-
-os.system("cat 2007_train.txt 2007_val.txt 2012_train.txt 2012_val.txt > train.txt")
-os.system("cat 2007_train.txt 2007_val.txt 2007_test.txt 2012_train.txt 2012_val.txt > train.all.txt")
+  
 
