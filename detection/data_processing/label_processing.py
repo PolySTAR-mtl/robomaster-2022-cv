d4ab860c8285071f8491dacdf52c407872ce9186
Sebastien Darche <sebastien.darche@free.fr>
2021-11-24 16:20:23 -0500

    Doc & some line returns for readability

--- detection/data_processing/label_processing.py
+++ detection/data_processing/label_processing.py
@@ -1,5 +1,15 @@
-# Copy of darknet/script/voc_label.py
-# Need to modify to process the .xml accordingly
+#!/usr/bin/env python
+
+"""
+@file label_processing.py
+@brief Process labels to produce darknet label formats from xml
+
+@details Copy of darknet/script/voc_label.py. Need to modify to process the .xml accordingly
+
+@author FlowSs <ton mail ici>
+"""
+
+
 import xml.etree.ElementTree as ET
 import pickle
 import os
@@ -7,9 +17,12 @@ from os import listdir, getcwd
 from os.path import join, isfile, splitext
 
 # Directories name
+
 sets = ['robomaster_Central China Regional Competition', 'robomaster_Final Tournament',
         'robomaster_North China Regional Competition', 'robomaster_South China Regional Competition']
+
 # List of classes
+
 classes = ["red_armor", "blue_armor", "grey_armor", "base", "watcher", "car"]
 
 
@@ -48,17 +61,23 @@ def convert_annotation(set_name, image_id):
         difficult = 0
         if obj.find('difficulty') is not None:
             difficult = obj.find('difficulty').text
+
         cls = obj.find('name').text
         cls_color = ""
+
         if obj.find('armor_color') is not None:
             cls_color = obj.find('armor_color').text + "_"
+
         if cls_color+cls not in classes or int(difficult) > 1:
             continue
+
         cls_id = classes.index(cls_color+cls)
         xmlbox = obj.find('bndbox')
+
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(
             xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
         bb = convert((w, h), b)
+
         out_file.write(str(cls_id) + " " +
                        " ".join([str(a) for a in bb]) + '\n')
 
@@ -69,18 +88,25 @@ if __name__ == "__main__":
 
     for set_name in sets:
         print("Processing %s..." % (set_name))
+
         if not os.path.exists('../dataset/%s' % (set_name)):
             print("No directory %s" % (set_name))
             continue
+
         if not os.path.exists('../dataset/%s/labels/' % (set_name)):
             os.makedirs('../dataset/%s/labels/' % (set_name))
+
         image_ids = [f for f in listdir(
             '../dataset/%s/image/' % (set_name)) if isfile(join('../dataset/%s/image/' % (set_name), f))]
+
         image_ids_ = [splitext(f)[0] for f in image_ids]
+
         list_file = open('../dataset/%s/%s.txt' % (set_name, set_name), 'w')
+
         for i, image_id in enumerate(image_ids_):
             if((i/len(image_ids)*100) % 10 == 0):
                 print("{:0.2f}% done".format(i/len(image_ids)*100))
+
             if(convert_annotation(set_name, image_id)):
                 list_file.write('../dataset/%s/image/%s.jpg\n' %
                                 (set_name, image_id))
