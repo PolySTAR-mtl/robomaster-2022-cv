8a0738def0829f9876daa259d6f3149a41c82510
Sebastien Darche <sebastien.darche@free.fr>
2022-02-09 18:07:58 -0500

    Detector stub, darknet setup

--- ros_ws/src/detection/src/detector.cpp
+++ ros_ws/src/detection/src/detector.cpp
@@ -5,3 +5,24 @@
  */
 
 #include "detector.hpp"
+
+// Darknet
+
+#include "darknet.h"
+
+struct Detector::impl {
+    network net;
+};
+
+Detector::Detector(ros::NodeHandle& n, const std::string& datacfg,
+                   const std::string& config_path,
+                   const std::string& weights_path)
+    : nh(n), p(std::make_unique<impl>()) {
+    setupNet(datacfg, config_path, weights_path);
+}
+
+void Detector::setupNet(const std::string& datacfg,
+                        const std::string& config_path,
+                        const std::string& weights_path) {}
+
+void Detector::imageCallback(const sensor_msgs::ImagePtr& img) {}
