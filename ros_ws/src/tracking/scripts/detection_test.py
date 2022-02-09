8537cd2aa6885005299dcdb39c21dcd910295209
Tzu-yi CHIU <tzuyi.chiu@gmail.com>
2022-02-09 01:17:26 -0500

    [to be tested] Tracking node with testing detections with YOLOv4

---
+++ ros_ws/src/tracking/scripts/detection_test.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+"""
+@file detection_test.py
+
+@author Tzu-yi Chiu <tzuyi.chiu@gmail.com>
+"""
+
+import os
+import cv2
+import numpy as np
+from motpy.tracker import Tracker
+from detection.msg import Detections, Detection as DetectionROS
+
+PROJECT_FOLDER = os.path.dirname(os.path.realpath(__file__))
+DATASET_FOLDER = os.path.join(PROJECT_FOLDER, 'TP3_data')
+VIDEO_FOLDER = os.path.join(DATASET_FOLDER, 'frames')
+YOLO_FOLDER = os.path.join(PROJECT_FOLDER, 'yolo')
+WEIGHTS_URL = 'https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights'
+WEIGHTS_PATH = os.path.join(YOLO_FOLDER, 'yolov4.weights')
+CONFIG_URL = 'https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg'
+CONFIG_PATH = os.path.join(YOLO_FOLDER, 'yolov4.cfg')
+
+### YOLO ###
+if not os.path.exists(YOLO_FOLDER):
+    os.mkdir(YOLO_FOLDER)
+if not os.path.isfile(WEIGHTS_PATH):
+    logger.debug('downloading weights to' + WEIGHTS_PATH)
+    urlretrieve(WEIGHTS_URL, WEIGHTS_PATH)
+if not os.path.isfile(CONFIG_PATH):
+    logger.debug('downloading config to' + CONFIG_PATH)
+    urlretrieve(CONFIG_URL, CONFIG_PATH)
+net = cv2.dnn.readNetFromDarknet(CONFIG_PATH, WEIGHTS_PATH)
+
+def init_detections():
+    """Loads the bbox provided in TP3_data/init.txt for the 1st frame
+    """
+    with open(os.path.join(DATASET_FOLDER, 'init.txt'), 'r') as f:
+        lines = f.readlines()
+    detections = []
+    for line in lines:
+        x, xmax, y, ymax = map(int, line.split()[2:6])
+        detection = DetectionROS(x=x, y=y, w=xmax-x, h=ymax-y, confidence=1)
+        detections.append(Detection(box=box,))
+    return Detections(detections=detections, frame=1)
+
+def yolo_detections(frame, img, threshold=0.3):
+    """Generates bbox detections with yolo-v4.
+    Adapted from https://github.com/gabilodeau/INF6804/blob/master/yolo_example.ipynb
+    """
+    h, w = img.shape[:2]
+    scale = np.array([w, h, w, h])
+    blob = cv2.dnn.blobFromImage(img, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    layer_outputs = net.forward(ln)
+
+    detections = []
+    for outputs in layer_outputs:
+        for output in outputs:
+            scores = output[5:]
+            class_id = np.argmax(scores)
+            conf = scores[class_id]
+            if conf > threshold and class_id == 41: # cup's class id is 41
+                box = output[:4] * scale
+                (centerX, centerY, w, h) = box.astype('int')
+                x = int(centerX - (w / 2))
+                y = int(centerY - (h / 2))
+                detection = DetectionROS(x=x, y=y, w=w, h=h, confidence=conf)
+                detections.append(detection)
+    return Detections(detections=detections, frame=frame)
+
+def main():
+    init = init_detections()
+    pub = rospy.Publisher('detections', Detections, queue_size=1)
+    rospy.init_node('detection_test', anonymous=False)
+    r = rospy.Rate(24)
+    nbframe = len(os.listdir(VIDEO_FOLDER))
+    imgs = (cv2.imread(os.path.join(VIDEO_FOLDER, f'frame{frame}.jpg')) 
+                for frame in range(1, nbframe+1))
+    frames = iter(range(1, nbframe+1))
+
+    while not rospy.is_shutdown():
+        frame = next(frames)
+        img = next(imgs)
+        if frame == 1:
+            detections = init_detections()
+        else:
+            try:
+                detections = yolo_detections(frame, img)
+            except StopIteration:
+                break
+        rospy.spin()
+        pub.publish(detections)
+        r.sleep()
+
+if __name__ == '__main__':
+    main()
+    
--- ros_ws/src/tracking/scripts/track.py
+++ ros_ws/src/tracking/scripts/track.py
@@ -2,32 +2,67 @@
 """
 @file track.py
 
-@author Sébastien Darche <sebastien.darche@polymtl>
+@author Tzu-yi Chiu <tzuyi.chiu@gmail.com>
 """
+
+PROJECT_FOLDER = os.path.dirname(os.path.realpath(__file__))
+DATASET_FOLDER = os.path.join(PROJECT_FOLDER, 'TP3_data')
+VIDEO_FOLDER = os.path.join(DATASET_FOLDER, 'frames')
 
 import rospy
 
 from sensor_msgs.msg import Image
-from detection.msg import Detections, Detection
+from detection.msg import Detections, Detection as DetectionROS
 from tracking.msg import Tracklets, Tracklet
 
+from motpy import MultiObjectTracker, Track, Detection as DetectionMOT
 
 class Tracker:
-
     def __init__(self):
-        self.sub_detection = rospy.Subscriber(
-            'detections_in', Image, self.detections_callback, queue_size=1)
+        # ROS
+        self.sub = rospy.Subscriber(
+            'detections_in', Detections, self.detections_callback, queue_size=1)
 
-        self.pub_tracks = rospy.Publisher('tracks', Tracklets, queue_size=1)
+        self.pub = rospy.Publisher('tracks', Tracklets, queue_size=1)
 
-    def detections_callback(self, dets: Detections):
-        return
+        # motpy - no confidence with the motion model (0.1)
+        #       - more confidence with detections (5000)
+        model_spec = {'order_pos': 1, 'dim_pos': 2,
+                    'order_size': 0, 'dim_size': 2,
+                    'q_var_size': 100., 'r_var_size': 10.,
+                    'q_var_pos': 5000., 'r_var_pos': 0.1}
+        
+        self.mot = MultiObjectTracker(model_spec=model_spec)
 
+    def detections_callback(self, detections_ros: Detections) -> Tracklets:
+        detections_mot = [self.det_ros2mot(detection_ros) 
+                            for detection_ros in detections_ros.detections]
+        tracks = self.mot.step(detections_mot)
+        tracklets = Tracklets([self.trk_mot2ros(track) for track in tracks])
+        self.pub.publish(tracklets)
 
-if __name__ == '__main__':
+        frame = detections_ros.frame
+        img = cv2.imread(os.path.join(VIDEO_FOLDER, f'frame{frame}.jpg'))
+        for det in detections_mot:
+            img = draw_rectangle(img, det.box, color=(0, 255, 0), thickness=2)
+        for trk in tracks:
+            img = draw_rectangle(img, det.box, color=(255, 0, 0), thickness=5)
+        cv2.imshow('Main', cv2.resize(img, (960, 540)))
+        
+    def det_ros2mot(self, detection: DetectionROS) -> DetectionMOT:
+        box = np.array([detection.x, detection.y, 
+                        detection.x + detection.w, detection.y + detection.h])
+        return DetectionMOT(box=box)
+
+    def trk_mot2ros(self, track: Track) -> Tracklet:
+        tracklet = Tracklet(box=list(track.box))
+        return tracklet
+
+def main():            
     rospy.init_node('tracking', anonymous=False)
     tracker = Tracker()
-
     while not rospy.is_shutdown():
         rospy.spin()
 
+if __name__ == '__main__':
+    main()
