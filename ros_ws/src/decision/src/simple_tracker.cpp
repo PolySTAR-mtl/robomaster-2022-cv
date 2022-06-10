fd3471d74df82586bc3cdb2544b446c82a48aa03
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-06-10 14:29:06 -0400

    clang-format

--- ros_ws/src/decision/src/simple_tracker.cpp
+++ ros_ws/src/decision/src/simple_tracker.cpp
@@ -12,17 +12,19 @@
 
 #include <ros/ros.h>
 
-#include "tracking/Tracklets.h"
 #include "serial/Target.h"
+#include "tracking/Tracklets.h"
 
 class SimpleTracker {
   public:
-    SimpleTracker(ros::NodeHandle& n, int _enemy_color) : nh(n), enemy_color(_enemy_color) {
+    SimpleTracker(ros::NodeHandle& n, int _enemy_color)
+        : nh(n), enemy_color(_enemy_color) {
         sub_tracklets = nh.subscribe("tracklets", 1,
-                                      &SimpleTracker::callbackTracklets, this);
+                                     &SimpleTracker::callbackTracklets, this);
 
         pub_target = nh.advertise<serial::Target>("target", 1);
-	std::cout << "Enemy color set to be: " << (enemy_color == 0 ?  "red" : "blue") << "\n";
+        std::cout << "Enemy color set to be: "
+                  << (enemy_color == 0 ? "red" : "blue") << "\n";
     }
 
     void callbackTracklets(const tracking::TrackletsConstPtr& trks) {
@@ -53,12 +55,13 @@ class SimpleTracker {
     serial::Target toTarget(tracking::Tracklet& trk) {
         serial::Target target;
 
-	    std::cout << "Det : " << trk.x << " ( " << trk.w << " ) " << trk.y << " ( " << trk.h << " )\n";
+        std::cout << "Det : " << trk.x << " ( " << trk.w << " ) " << trk.y
+                  << " ( " << trk.h << " )\n";
 
         auto x_c = trk.x + trk.w / 2 - im_w / 2;
         auto y_c = trk.y + trk.h / 2 - im_h / 2;
 
-	    std::cout << "x_c = " << x_c << " ; y_c = " << y_c << '\n';
+        std::cout << "x_c = " << x_c << " ; y_c = " << y_c << '\n';
 
         uint16_t theta = std::floor((y_c * alpha_y + M_PI_2) * 1000.f);
         int16_t phi = std::floor(x_c * alpha_x * 1000.f);
@@ -91,7 +94,7 @@ class SimpleTracker {
 int main(int argc, char** argv) {
     ros::init(argc, argv, "decision");
     ros::NodeHandle nh("~");
-	
+
     int enemy_color;
 
     if (!nh.getParam("enemy_color", enemy_color)) {
